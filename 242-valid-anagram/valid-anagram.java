ffed4c5b586636499eb3e3fa62520951ffa499e5
letscodeasume <anujyadav7855@gmail.com>
2026-02-13 21:37:20 +0530

    Time: 3 ms (91.00%) | Memory: 44.2 MB (95.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int sl = s.length();
+        int tl = t.length();
+        if (sl != tl) {
+            return false;
+        }
+        int[] arr = new int[26];
+        for (int i = 0; i < sl; i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < tl; i++) {
+            arr[t.charAt(i) - 'a']--;
+            if (arr[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
