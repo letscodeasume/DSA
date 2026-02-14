86952e87ef402387c3a6bcbd568f9416a85cf360
letscodeasume <anujyadav7855@gmail.com>
2026-02-14 22:18:39 +0530

    Time: 7 ms (76.68%) | Memory: 49.6 MB (85.52%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,35 +1,53 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return new ArrayList<>();
-        }
-        Map<String, List<String>> freqmp = new HashMap<>();
-        for (String s : strs) {
-            String freqst = getfreqst(s);
-            if (freqmp.containsKey(freqst)) {
-                freqmp.get(freqst).add(s);
-            } else {
-                List<String> st = new ArrayList<>();
-                st.add(s);
-                freqmp.put(freqst, st);
-            }
-        }
-        return new ArrayList<>(freqmp.values());
-    }
+        //         if (strs == null || strs.length == 0) {
+        //             return new ArrayList<>();
+        //         }
+        //         Map<String, List<String>> freqmp = new HashMap<>();
+        //         for (String s : strs) {
+        //             String freqst = getfreqst(s);
+        //             if (freqmp.containsKey(freqst)) {
+        //                 freqmp.get(freqst).add(s);
+        //             } else {
+        //                 List<String> st = new ArrayList<>();
+        //                 st.add(s);
+        //                 freqmp.put(freqst, st);
+        //             }
+        //         }
+        //         return new ArrayList<>(freqmp.values());
+        //     }
 
-    private String getfreqst(String st) {
-        int[] freq = new int[26];
-        for (char c : st.toCharArray()) {
-            freq[c - 'a']++;
-        }
-        StringBuilder freqst = new StringBuilder("");
-        char c = 'a';
-        for (int i : freq) {
+        //     private String getfreqst(String st) {
+        //         int[] freq = new int[26];
+        //         for (char c : st.toCharArray()) {
+        //             freq[c - 'a']++;
+        //         }
+        //         StringBuilder freqst = new StringBuilder("");
+        //         char c = 'a';
+        //         for (int i : freq) {
 
-            freqst.append(c);
-            freqst.append(i);
-            c++;
+        //             freqst.append(c);
+        //             freqst.append(i);
+        //             c++;
+        //         }
+        //         return freqst.toString();
+        //     }
+        // }
+
+        // public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
         }
-        return freqst.toString();
+
+        return new ArrayList<>(map.values());
     }
 }
